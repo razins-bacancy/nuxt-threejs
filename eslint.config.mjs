777cb08687ac4626c12ab29c55a2c2e14233e1f4
razins-bacancy <mohammedrazin.shaikh@bacancy.com>
2025-07-31 16:11:39 +0530

    eslint module added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(
-  // Your custom configs here
-)
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default withNuxt({
+  ...eslintPluginPrettierRecommended,
+});
